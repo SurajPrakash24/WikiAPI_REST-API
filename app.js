6c504ef3db1a840b8af6b46cfe195024e2a52be3
SurajPrakash <surajprakashhh04@gmail.com>
2023-02-15 12:11:18 +0530

    replaced different route methods with chain route handler using express

--- app.js
+++ app.js
@@ -22,39 +22,42 @@ const articleSchema = {
 // Created Article model using mongoose
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles",function(req, res){
-  Article.find(function(err, foundArticles){
-    if(!err){
-      res.send(foundArticles);
-    }else{
-      res.send(err);
-    }
-  });
-});
+// chain route handler using express instead of different route methods
+app.route("/articles")
+  .get(function(req, res){
+    Article.find(function(err, foundArticles){
+      if(!err){
+        res.send(foundArticles);
+      }else{
+        res.send(err);
+      }
+    });
+  })
 
-app.post("/articles", function(req, res){
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newArticle.save(function(err){
-    if(!err){
-      res.send("Successfully added a new article.")
-    }else{
-      res.send(err);
-    }
-  });
-});
+  .post(function(req, res){
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err){
+      if(!err){
+        res.send("Successfully added a new article.")
+      }else{
+        res.send(err);
+      }
+    });
+  })
 
-app.delete("/articles", function(req, res){
-  Article.deleteMany(function(err){
-    if(!err){
-      res.send("Successfully deleted all articles");
-    }else{
-      res.send(err);
-    }
+  .delete(function(req, res){
+    Article.deleteMany(function(err){
+      if(!err){
+        res.send("Successfully deleted all articles");
+      }else{
+        res.send(err);
+      }
+    });
   });
-});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
