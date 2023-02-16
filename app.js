78bc7e699c7b4651156f25bad2e40b19ab24ec69
SurajPrakash <surajprakashhh04@gmail.com>
2023-02-17 01:52:42 +0530

    Added requests targetting a specific item

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 // chain route handler using express instead of different route methods
+/* Request Targetting All Articles */
 app.route("/articles")
   .get(function(req, res){
     Article.find(function(err, foundArticles){
@@ -58,6 +59,57 @@ app.route("/articles")
     });
   });
 
+
+/* Request Targetting a specific Articles */
+app.route("/articles/:articleTitle")
+  .get(function(req, res){
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+      if(foundArticle){
+        res.send(foundArticle);
+      }else{
+        res.send("Article not found with given Title.")
+      }
+    });
+  })
+  .put(function(req, res){
+    Article.findOneAndUpdate(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      function(err){
+        if(!err){
+          res.send("Successfully updated the selected article.");
+        }else{
+          res.send("Unsuccessful");
+        }
+      }
+    );
+  })
+  .patch(function(req, res){
+    Article.findOneAndUpdate(
+      {title: req.params.articleTitle},
+      req.body,
+      function(err){
+        if(!err){
+          res.send("Successfully updated article.");
+        }else{
+          res.send(err);
+        }
+      }
+    );
+  })
+  .delete(function(req, res){
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      function(err){
+        if(!err){
+          res.send("Successfully deleted the corresponding article.");
+        }else{
+          res.send(err);
+        }
+      }
+    );
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
